8af5df65ab5754fda2475968d7c18b8a5b442d94
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-06 11:30:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,34 @@ const app = express()
 
 // Your code goes here
 
+const Subscriber=require('./models/subscribers');
+
+
+app.get("/subscribers",(req,res)=>{
+    Subscriber.find().then((ele)=>{
+        res.send(ele);
+        return;
+    })
+})
+
+
+app.get("/subscribers/names",(req,res)=>{
+    Subscriber.find().select({name:1,subscribedChannel:1}).then((ele)=>{
+        res.send(ele);
+        return;
+    })
+})
+
+
+app.get("/subscribers/:id",(req,res)=>{
+    const id=req.params.id;
+    Subscriber.find({_id:{$eq:id}}).then((ele)=>{
+        res.send(ele);
+        return;
+    })
+})
+
+
 
 
 
